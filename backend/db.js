08fbaeb6266ae22e3ddf21cf17c118ba71f5a541
Santiago Agonil <54754787+NessPk@users.noreply.github.com>
2024-10-12 00:37:54 -0300

    Revert "Conexion, routes y controllers, GET y POST hechos"

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-import { createPool } from 'mysql12/promise'
-
-export const pool = createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'arifureta123$',
-    port: 3306,
-    database: 'mantenimiento'
-})
