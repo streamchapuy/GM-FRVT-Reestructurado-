d87947d0879909ce027e561fe3c9bb2b2e3eeca1
streamchapuy <federicobraga1234@gmail.com>
2024-10-15 12:41:50 -0300

    Se agrego autenticacion

---
+++ backend/controllers/login.aut.controller.js
@@ -0,0 +1,38 @@
+import bcript from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import base_conexion from '../db';
+
+
+export const  logeoUser = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const query = 'SELECT * FROM users WHERE username = ?';
+        base_conexion.query(query, [username], async (error, results) => {
+            if (error) {
+                console.error('Error al buscar el usuario: ' + error);
+                return res.status(500).json({ message: 'Error al buscar el usuario' });
+            }
+
+            if (results.length === 0) {
+                return res.status(401).json({ message: 'Credenciales inválidas' });
+            }
+            const user = results[0];
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(401).json({ message: 'Credenciales inválidas' });
+            }
+
+            const token = jwt.sign({ userId: user.id }, SECRET_KEY, { expiresIn: '1h' });
+            res.json({ token });
+
+            res.json({ message: 'Inicio de sesión exitoso', token });
+        });
+
+
+    } catch (error) {
+        console.error('Error en el proceso de login: ' + error);
+        res.status(500).json({ message: 'Error en el proceso de login' })
+    }
+}
---
+++ backend/middleware/authmiddleware.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const secretKey = process.env.SECRET_KEY || "default_key";
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401).json({error: 'Token no proporcionado'});
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.sendStatus(403).json({error: 'Token inválido'});
+    
+    req.user = user;
+    next();
+  });
+};
+export default authenticateToken;
