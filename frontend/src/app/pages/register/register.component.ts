84c8bcce9303413f0011814599a9a503237d538d
NessPk <sanjixa1693@gmail.com>
2024-10-21 20:52:17 -0300

    Register frontend creado y linkeado con login y viceversa

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  email: string | undefined;
+  password: string | undefined;
 
+  constructor(private router: Router) {}
+
+  register() {
+    throw new Error('Method not implemented.');
+  }
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
