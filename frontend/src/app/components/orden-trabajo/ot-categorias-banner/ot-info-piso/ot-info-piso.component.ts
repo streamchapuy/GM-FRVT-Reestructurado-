40d0628fdac526964888edc1c3ab6bcae81d046e
NessPk <sanjixa1693@gmail.com>
2024-10-25 01:01:24 -0300

    Funciones ot-categorias-banner terminado

--- frontend/src/app/components/orden-trabajo/ot-categorias-banner/ot-info-piso/ot-info-piso.component.ts
+++ frontend/src/app/components/orden-trabajo/ot-categorias-banner/ot-info-piso/ot-info-piso.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OtPisoService } from '../../../../../services/ot-piso.service';
+import { Piso } from '../../../../interfaces/piso';
 
 @Component({
   selector: 'app-ot-info-piso',
   templateUrl: './ot-info-piso.component.html',
   styleUrl: './ot-info-piso.component.css'
 })
-export class OtInfoPisoComponent {
+export class OtInfoPisoComponent implements OnInit{
+  pisos: Piso[] = [];
+  selectorPiso: number | null = null;
+  constructor(private pisoService: OtPisoService) { }
 
+  ngOnInit(): void {
+    this.loadPisos();
+  }
+  loadPisos() {
+    this.pisoService.obtenerPiso().subscribe({
+      next: (data: Piso[]) => {
+        this.pisos = data;
+        console.log(this.pisos);
+      },
+      error: (err) => {
+        console.error('Error al cargar los pisos:', err);
+      }
+    });
+  }
 }
