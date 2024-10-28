fadd619404f9acbc01d24043ec8f7b2fbc5f82c5
Nacho <Cifuentes.Ignacio.1994@gmail.com>
2024-10-28 17:13:42 -0300

    CommitCifu

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { otComponent } from './components/orden-trabajo/ot/ot.component';
+import { FormActivoComponent } from './components/formularios/form-activo/form-activo.component';
+import { FormEdificioComponent } from './components/formularios/form-edificio/form-edificio.component';
+import { FormPisoComponent } from './components/formularios/form-piso/form-piso.component';
+import { FormSectorComponent } from './components/formularios/form-sector/form-sector.component';
+import { FormUbicacionComponent } from './components/formularios/form-ubicacion/form-ubicacion.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'orden', component: otComponent },  
+  { path: 'ordenTrabajo', component: otComponent },
+  { path: 'formActivo', component: FormActivoComponent},
+  { path: 'formEdificio', component: FormEdificioComponent},
+  { path: 'formPiso', component: FormPisoComponent},
+  { path: 'formSector', component: FormSectorComponent},
+  { path: 'formUbicacion', component: FormUbicacionComponent},
+  
   // { path: '', redirectTo: '/login', pathMatch: 'full' },
   // { path: '**', redirectTo: '/login' }
 ];
