a18640c0430ca5385a5b834994d2af33a2b0b411
NessPk <sanjixa1693@gmail.com>
2024-11-04 17:17:08 -0300

    Componente ot-fechas reestructurado y dividido. Ordenado los espacios de algunos otros componentes

---
+++ frontend/src/app/components/orden-trabajo/ot-banner-fecha/ot-tarea-operario/ot-tarea-operario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ot-tarea-operario',
+  templateUrl: './ot-tarea-operario.component.html',
+  styleUrls: ['./ot-tarea-operario.component.css']
+})
+export class OtTareaOperarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
