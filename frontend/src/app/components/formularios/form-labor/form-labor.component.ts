895f88a7e1cf349af9af083b5d445fc1c7ee609f
NessPk <sanjixa1693@gmail.com>
2024-11-08 20:03:35 -0300

    Home Page logueada con Forms Ots y Usuarios

---
+++ frontend/src/app/components/formularios/form-labor/form-labor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-labor',
+  templateUrl: './form-labor.component.html',
+  styleUrls: ['./form-labor.component.css']
+})
+export class FormLaborComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
