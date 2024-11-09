a0760d36ebb0b37679767bf6ebba15d99a8e5f28
NessPk <sanjixa1693@gmail.com>
2024-11-09 20:47:57 -0300

    Home-logged reestructurado

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { FooterModule } from './components/footer/footer.module';
@@ -11,11 +11,11 @@ import { HeaderModule } from './components/header/header.module';
 import { OrdenTrabajoModule } from './components/orden-trabajo/orden-trabajo.module';
 
 import { CookieService } from 'ngx-cookie-service';
+import { ToastrModule } from 'ngx-toastr';
 import { HomeLoggedComponent } from './pages/home-logged/home-logged.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
-import { ToastrModule } from 'ngx-toastr';
 
 
 
@@ -36,8 +36,7 @@ import { ToastrModule } from 'ngx-toastr';
     HeaderModule,
     FooterModule,
     BrowserAnimationsModule,
-    
-    ToastrModule
+    ToastrModule,
 ],
   providers: [CookieService],
   bootstrap: [AppComponent]
