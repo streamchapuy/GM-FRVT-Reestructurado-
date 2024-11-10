1f033554f2bf39722656f248e8038fb669669a89
Santiago Agonil <54754787+NessPk@users.noreply.github.com>
2024-11-09 22:54:54 -0300

    Revert "home-logged hecho, creado header-logged"

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 import { FooterModule } from './components/footer/footer.module';
 import { FormulariosModule } from './components/formularios/formularios.module';
-import { HeaderLoggedModule } from './components/header-logged/header-logged.module';
 import { HeaderModule } from './components/header/header.module';
 import { OrdenTrabajoModule } from './components/orden-trabajo/orden-trabajo.module';
 
 import { CookieService } from 'ngx-cookie-service';
-import { ToastrModule } from 'ngx-toastr';
 import { HomeLoggedComponent } from './pages/home-logged/home-logged.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ToastrModule } from 'ngx-toastr';
 
 
 
@@ -35,11 +34,9 @@ import { RegisterComponent } from './pages/register/register.component';
     OrdenTrabajoModule,
     FormulariosModule,
     HeaderModule,
-    HeaderLoggedModule,
     FooterModule,
-    BrowserAnimationsModule,
-    ToastrModule,
-
+    BrowserAnimationsModule,    
+    ToastrModule
 ],
   providers: [CookieService],
   bootstrap: [AppComponent]
