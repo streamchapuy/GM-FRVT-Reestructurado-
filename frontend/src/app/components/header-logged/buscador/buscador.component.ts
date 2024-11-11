27df72221f69ff85028c9ea65669600dbfcb7009
NessPk <sanjixa1693@gmail.com>
2024-11-11 16:27:49 -0300

    Buscador terminado

--- frontend/src/app/components/header-logged/buscador/buscador.component.ts
+++ frontend/src/app/components/header-logged/buscador/buscador.component.ts
@@ -32,9 +32,51 @@ export class BuscadorComponent {
     'usuario': '/formUsuarios',
     'tareas': '/formTareas',
     'tarea': '/formTareas',
+    'home': '/home-logged',
+    'adin': '/home-logged',
+    'pendientes': '/home-operario',
+    'ot pendientes': '/home-operario',
+    'ordenes pendientes': '/home-operario',
+    'ordenes de trabajo pendientes': '/home-operario',
+    'pendiente': '/home-operario',
+    'ot pendiente': '/home-operario',
+    'orden pendiente': '/home-operario',
+    'orden de trabajo pendiente': '/home-operario',
+    'activas': '/home-operario',
+    'ot activas': '/home-operario',
+    'ordenes activas': '/home-operario',
+    'ordenes de trabajo activas': '/home-operario',
+    'activa': '/home-operario',
+    'ot activa': '/home-operario',
+    'orden activa': '/home-operario',
+    'orden de trabajo activa': '/home-operario',
+    'terminadas': '/home-operario',
+    'ot terminadas': '/home-operario',
+    'ordenes terminadas': '/home-operario',
+    'ordenes de trabajo terminadas': '/home-operario',
+    'terminada': '/home-operario',
+    'ot terminada': '/home-operario',
+    'orden terminada': '/home-operario',
+    'orden de trabajo terminada': '/home-operario',
+    'finalizadas': '/home-operario',
+    'ot finalizadas': '/home-operario',
+    'ordenes finalizadas': '/home-operario',
+    'ordenes de trabajo finalizadas': '/home-operario',
+    'finalizada': '/home-operario',
+    'ot finalizada': '/home-operario',
+    'orden finalizada': '/home-operario',
+    'orden de trabajo finalizada': '/home-operario',
+    'canceladas': '/home-operario',
+    'ot canceladas': '/home-operario',
+    'ordenes canceladas': '/home-operario',
+    'ordenes de trabajo canceladas': '/home-operario',
+    'cancelada': '/home-operario',
+    'ot cancelada': '/home-operario',
+    'orden cancelada': '/home-operario',
+    'orden de trabajo cancelada': '/home-operario',
   };
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   onSearch(): void {
     const searchValue = this.searchValue.toLowerCase().trim();
@@ -46,6 +88,6 @@ export class BuscadorComponent {
       }
     }
 
-    console.log('No se encontró una ruta para el término de búsqueda');
+    this.router.navigate(['/404']);
   }
 }
