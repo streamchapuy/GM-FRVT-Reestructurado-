36ee46fc39b7869ffe2eb761d0713a5d752f8487
NessPk <sanjixa1693@gmail.com>
2024-11-20 19:54:56 -0300

    Creado componente "landing-informacion" con su estilo y propiedades.

---
+++ frontend/src/app/components/landing-informacion/landing-informacion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-informacion',
+  templateUrl: './landing-informacion.component.html',
+  styleUrls: ['./landing-informacion.component.css']
+})
+export class LandingInformacionComponent {
+
+}
