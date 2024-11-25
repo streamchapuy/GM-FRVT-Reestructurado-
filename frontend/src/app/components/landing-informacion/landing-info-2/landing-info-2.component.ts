eb52efae4ffa5328a0511e3f677cf776e6feee67
NessPk <sanjixa1693@gmail.com>
2024-11-25 01:35:12 -0300

    componente landing-info-2 terminado

---
+++ frontend/src/app/components/landing-informacion/landing-info-2/landing-info-2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-info-2',
+  templateUrl: './landing-info-2.component.html',
+  styleUrl: './landing-info-2.component.css'
+})
+export class LandingInfo2Component {
+
+}
