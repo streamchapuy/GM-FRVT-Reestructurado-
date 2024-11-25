5fc189a3e8e28e89c557d8078af514bf7b255915
NessPk <sanjixa1693@gmail.com>
2024-11-25 03:26:57 -0300

    landing-info-4 terminado

---
+++ frontend/src/app/components/landing-informacion/landing-info-4/landing-info-4.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-info-4',
+  templateUrl: './landing-info-4.component.html',
+  styleUrl: './landing-info-4.component.css'
+})
+export class LandingInfo4Component {
+
+}
