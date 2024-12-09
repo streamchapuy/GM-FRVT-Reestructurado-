83293c637b48fa45b527a94d4de01abb71da43a8
NessPk <sanjixa1693@gmail.com>
2024-12-09 19:36:12 -0300

    Conexión y selección de operario terminada

--- frontend/src/app/components/orden-trabajo/fechas/fechas.component.ts
+++ frontend/src/app/components/orden-trabajo/fechas/fechas.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UsuariosService } from '../../../../services/usuarios.service';
+import { Usuario } from '../../../interfaces/usuario';
 
 @Component({
   selector: 'app-fechas',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrl: './fechas.component.css'
 })
 export class FechasComponent {
+  usuarios: Usuario[] = [];
 
+  constructor(private usuariosService: UsuariosService) { }
+
+  ngOnInit(): void {
+    this.usuariosService.obtenerUsuarios().subscribe(
+      (data) => {
+        this.usuarios = data.filter((usuario) => usuario.tipo_usuario === 'operario');
+      },
+      (error) => {
+        console.error('Error al obtener usuarios:', error);
+      }
+    );
+  }
 }
