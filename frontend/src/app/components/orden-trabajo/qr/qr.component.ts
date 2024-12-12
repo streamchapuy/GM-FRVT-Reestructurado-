f133e8f2334769abe6f63e868162b944f23975b1
Pato mafioso <137842633+pato-mafioso@users.noreply.github.com>
2024-12-11 21:27:07 -0300

    QR hecho y funcional

---
+++ frontend/src/app/components/orden-trabajo/qr/qr.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { QrService } from '../../../../services/qr.service';
+
+@Component({
+  selector: 'app-qr',
+  templateUrl: './qr.component.html',
+  styleUrl: './qr.component.css'
+})
+export class QRComponent implements OnInit {
+  qrCodeImage: string = '';
+
+  constructor(private qrService: QrService) {}  // Inyecta el servicio
+
+  ngOnInit() {
+    this.generateQRCode('http://localhost:4200/ordenTrabajo');
+    this.generateQRCode('http://192.168.0.106:4200/ordenTrabajo');
+  }
+
+  async generateQRCode(data: string) {
+    try {
+      this.qrCodeImage = await this.qrService.generateQRCode(data);  // Llama al servicio
+    } catch (err) {
+      console.error('Error al generar QR:', err);
+    }
+  }
+}
